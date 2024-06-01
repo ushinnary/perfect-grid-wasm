4acb5d63b8ea3c30f3c9a6ecfa3df41e849e1ddf
Ushinnary <kartushin.alexander97@gmail.com>
2024-06-01 23:12:05 +0200

    Update dep versions

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,11 @@ impl ImageGrid {
     }
 
     // Getting sum of all elements after multiply
-    fn calculate_all_width_by_height(&self, ratios: &[f64], desired_height: f64) -> f64 {
+    fn calculate_all_width_by_height(
+        &self,
+        ratios: &[f64],
+        desired_height: f64,
+    ) -> f64 {
         ratios.iter().fold(-self.gap, |acc, &ratio| {
             acc + desired_height * ratio + self.gap
         })
@@ -59,14 +63,17 @@ impl ImageGrid {
         ratios: &[f64],
         desired_height: f64,
     ) -> Result<f64, ResizeError> {
-        if ratios
-            .iter()
-            .any(|ratio| (desired_height * ratio) + self.gap < self.min_item_width)
-        {
+        if ratios.iter().any(|ratio| {
+            (desired_height * ratio) + self.gap
+                < self.min_item_width
+        }) {
             return Err(ResizeError::MinItemWidthOverload);
         }
 
-        let width = self.calculate_all_width_by_height(ratios, desired_height);
+        let width = self.calculate_all_width_by_height(
+            ratios,
+            desired_height,
+        );
 
         if width > self.available_width {
             Err(ResizeError::CanNotFitItems)
@@ -76,39 +83,59 @@ impl ImageGrid {
     }
 
     /// Check if we may fit all items with max or min line height
-    fn items_may_be_fitted(&self, ratios: &[f64], height: f64) -> bool {
-        self.calculate_all_width_by_height_secure(ratios, height)
-            .is_ok()
+    fn items_may_be_fitted(
+        &self,
+        ratios: &[f64],
+        height: f64,
+    ) -> bool {
+        self.calculate_all_width_by_height_secure(
+            ratios, height,
+        )
+        .is_ok()
     }
 
     /// Returns vector of tuples with number of items to take and height for them
-    pub fn get_row_from_items(&self, ratios: &mut Vec<f64>) -> Vec<(u32, f64)> {
+    pub fn get_row_from_items(
+        &self,
+        ratios: &mut Vec<f64>,
+    ) -> Vec<(u32, f64)> {
         let mut not_fitted: Vec<f64> = Vec::new();
-        let mut height_for_ratios = self.get_optimal_height(ratios);
+        let mut height_for_ratios =
+            self.get_optimal_height(ratios);
 
         for ratio_index in 0..ratios.len() {
             let items = &ratios[0..=ratio_index];
             let new_height = self.get_optimal_height(items);
-            if !self.items_may_be_fitted(items, new_height) {
-                not_fitted = ratios.drain(ratio_index..).collect();
+            if !self.items_may_be_fitted(items, new_height)
+            {
+                not_fitted =
+                    ratios.drain(ratio_index..).collect();
                 break;
             }
 
-            height_for_ratios = new_height.min(self.max_line_height);
+            height_for_ratios =
+                new_height.min(self.max_line_height);
         }
 
-        let mut result = vec![(ratios.len() as u32, height_for_ratios)];
+        let mut result =
+            vec![(ratios.len() as u32, height_for_ratios)];
 
         if !not_fitted.is_empty() {
-            let rest_filtered = &mut self.get_row_from_items(&mut not_fitted);
+            let rest_filtered = &mut self
+                .get_row_from_items(&mut not_fitted);
             result.append(rest_filtered);
         }
 
         result
     }
 
-    pub fn get_optimal_height(&self, ratios: &[f64]) -> f64 {
-        let gaps_width = ratios.len().saturating_sub(1) as f64 * self.gap;
+    pub fn get_optimal_height(
+        &self,
+        ratios: &[f64],
+    ) -> f64 {
+        let gaps_width = ratios.len().saturating_sub(1)
+            as f64
+            * self.gap;
         let total_width = self.available_width - gaps_width;
         (total_width / ratios.iter().sum::<f64>()).floor()
     }
@@ -132,10 +159,13 @@ pub fn get_optimal_grid(
         gap,
     );
 
-    let height_list = grid.get_row_from_items(&mut ratios).into_iter();
+    let height_list =
+        grid.get_row_from_items(&mut ratios).into_iter();
 
     height_list
-        .flat_map(|(count, height)| std::iter::repeat(height).take(count as usize))
+        .flat_map(|(count, height)| {
+            std::iter::repeat(height).take(count as usize)
+        })
         .map(JsValue::from)
         .collect::<js_sys::Array>()
 }
